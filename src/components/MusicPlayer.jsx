81c939fe810ea26ab358aea7e2eb11bd484179ab
grahacr <courtney.graham@atlasschool.com>
2025-01-20 22:47:11 -0600

    setting up and practicing tailwind to build bones of music player. need to separate out into components each piece

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,3 +1,15 @@
 export default function MusicPlayer() {
-  return <p>Music player goes here</p>;
+  return (
+    <div className="flex flex-row w-full">
+      <div className="flex-1 p-4 bg-gray-200">
+        <img src="../src/assets/placeholder.svg" alt="song image" />
+        <p>Song Title</p>
+        <p>artist</p>
+      </div>
+
+      <div className="flex-1 p-4 bg-gray-100">
+        <h2>Playlist</h2>
+      </div>
+    </div>
+    );
 }
