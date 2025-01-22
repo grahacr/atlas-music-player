557ad504712dc49f3db960393184d24d7e3dd066
grahacr <courtney.graham@atlasschool.com>
2025-01-21 20:30:38 -0600

    covertart component renders placeholder cover art for now, not perfectly sized yet

---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,8 @@
+// covert art component renders song image
+export default function CoverArt() {
+    return (
+    <div className="bg-gray-200 w-6/12 max-h-fit">
+        <img src="../src/assets/placeholder.svg" alt="song image" />
+    </div>
+    )
+}
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,14 +1,10 @@
+import CoverArt from "./CoverArt";
+
 export default function MusicPlayer() {
   return (
-    <div className="flex flex-row w-full">
-      <div className="flex-1 p-4 bg-gray-200">
-        <img src="../src/assets/placeholder.svg" alt="song image" />
-        <p>Song Title</p>
-        <p>artist</p>
-      </div>
-
-      <div className="flex-1 p-4 bg-gray-100">
-        <h2>Playlist</h2>
+    <div className="flex flex-row">
+      <CoverArt />
+      <div className="bg-gray-100">
       </div>
     </div>
     );
