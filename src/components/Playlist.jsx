2902c1026460ab34c3abc77d198aed89796a78c2
grahacr <courtney.graham@atlasschool.com>
2025-01-22 22:30:06 -0600

    getting closer with playlist item using grid system

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,10 +1,12 @@
+// playlist component
+
+import PlaylistItem from "./PlaylistItem"
+
 export default function Playlist() {
     return (
-        <div className="space-y-2">
-            <div className="bg-white p-2 rounded-lg shadow-sm">Song 1</div>
-            <div className="bg-white p-2 rounded-lg shadow-sm">Song 2</div>
-            <div className="bg-white p-2 rounded-lg shadow-sm">Song 3</div>
-
+        <div className="flex flex-col bg-white p-2 rounded-lg">
+            <h4 className="font-medium">Playlist</h4>
+            <PlaylistItem />
         </div>
-        )
+        );
 }
---
+++ src/components/PlaylistItem.jsx
@@ -0,0 +1,11 @@
+export default function PlaylistItem() {
+    return (
+        <div className="grid grid-cols-2">
+            <h2>hello</h2>
+                <div className="justify-items-end">
+                    <h3 className="">5:55</h3>
+                </div>
+            <h3>wow</h3>
+        </div>
+        );
+}
