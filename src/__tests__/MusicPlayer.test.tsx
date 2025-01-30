f91375cf4f041c5757aed6a5e34c7330f1374163
grahacr <courtney.graham@atlasschool.com>
2025-01-29 23:29:25 -0600

    started set up for music player tests but still having issue with msw working properly and recognizing expect function

--- src/__tests__/MusicPlayer.test.tsx
+++ src/__tests__/MusicPlayer.test.tsx
@@ -1,8 +1,9 @@
+import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
-import { http, HttpResponse} from "msw";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { test, beforeAll, afterEach, afterAll } from "vitest";
 import MusicPlayer from "../components/MusicPlayer";
-
+import { PlayerProvider } from "../components/PlayerContext.tsx";
 
 const mockPlaylist = [
     {
@@ -29,7 +30,7 @@ const mockPlaylist = [
 ];
 
 export const handlers = [
-    http.get('/api/v1/playlist', () => {
+    http.get("/api/v1/playlist", () => {
         return HttpResponse.json(mockPlaylist);
     }),
 ];
@@ -41,10 +42,44 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 test("MusicPlayer fetches and renders playlist correctly", async () => {
-    render(<MusicPlayer />);
+    render(
+        <PlayerProvider>
+    <MusicPlayer />
+    </PlayerProvider>
+    );
     await waitFor(() => screen.getByText("Painted in Blue"));
 
     expect(screen.getByText("Painted in Blue")).toBeInTheDocument();
     expect(screen.getByText("Tidal Drift")).toBeInTheDocument();
     expect(screen.getByText("Fading Shadows")).toBeInTheDocument();
 });
+
+test("Verify current song is first song in playlist by default", async () => {
+    render(<PlayerProvider>
+        <MusicPlayer />
+        </PlayerProvider>);
+
+    await waitFor(() => screen.getByText("Painted in Blue"));
+
+    expect(screen.getByText("Painted in Blue")).toBeInTheDocument();
+});
+
+test("Verify play/pause can be toggled", async () => {
+    render(
+    <PlayerProvider>
+        <MusicPlayer />
+    </PlayerProvider>);
+
+    const titles = await screen.findAllByText("Painted in Blue");
+    expect(titles).toHaveLength(1);
+
+    const playPauseButton = screen.getByRole("button", { name: /play/i });
+
+    fireEvent.click(playPauseButton);
+
+    expect(playPauseButton).toHaveTextContent("Pause");
+
+    fireEvent.click(playPauseButton);
+
+    expect(playPauseButton).toHaveTextContent("Play");
+})
